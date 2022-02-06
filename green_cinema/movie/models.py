13ee82648673abcdd0826d40423a888a6b4cdf3f
Github <weeds1590@gmail.com>
2022-02-06 21:02:41 +0900

    fix - movie.Rating.rating choices 속성 수정

--- green_cinema/movie/models.py
+++ green_cinema/movie/models.py
@@ -15,22 +15,23 @@ class Movie(models.Model):
     cast = models.CharField(max_length=200, blank=True)
     film_rating_system = models.CharField(max_length=20, blank=True)
     information = models.TextField(blank=True)
+    information_url = models.URLField(max_length=200, blank=True)
 
 
 class Rating(models.Model):
     class Meta:
         db_table = "rating"
 
-    RATING1 = '1'
-    RATING2 = '2'
-    RATING3 = '3'
-    RATING4 = '4'
-    RATING5 = '5'
-    RATING6 = '6'
-    RATING7 = '7'
-    RATING8 = '8'
-    RATING9 = '9'
-    RATING10 = '10'
+    RATING1 = '0.5'
+    RATING2 = '1'
+    RATING3 = '1.5'
+    RATING4 = '2'
+    RATING5 = '2.5'
+    RATING6 = '3'
+    RATING7 = '3.5'
+    RATING8 = '4'
+    RATING9 = '4.5'
+    RATING10 = '5'
 
     STAR_CHOICES = [
         (RATING1, 1),
@@ -47,5 +48,5 @@ class Rating(models.Model):
 
     user_id = models.ForeignKey('user.UserModel', related_name="user", on_delete=models.CASCADE, db_column="user_id")
     movie_id = models.ForeignKey('Movie', related_name="movie", on_delete=models.CASCADE, db_column="movie_id")
-    rating = models.CharField(max_length=2, choices=STAR_CHOICES, default=RATING10)
+    rating = models.CharField(max_length=3, choices=STAR_CHOICES, default=RATING10)
     comment = models.CharField(max_length=500, default='', blank=True)
