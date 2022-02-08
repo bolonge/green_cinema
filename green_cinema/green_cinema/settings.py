0f1775f9cb840942df8df78f73a279a4457f33dd
Elijahrong <nhkim5760@gmail.com>
2022-02-08 20:00:49 +0900

    edit profile success!!

--- green_cinema/green_cinema/settings.py
+++ green_cinema/green_cinema/settings.py
@@ -137,3 +137,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.UserModel' 
+
+# # send email
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.googlemail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = '2022have12@gmail.com'
+# EMAIL_HOST_PASSWORD = '12have12!'
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
